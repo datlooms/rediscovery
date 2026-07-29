7f57579ef46796773fbb92cbe98e9fe2e2e59f7b
datlooms <d@looms.info>
2026-07-29 08:15:07 +0700

    update

--- dot_master_discovery/sitecustomize.py
+++
@@ -1,6 +0,0 @@
-try:
-    import dot_frame_binding
-except Exception:
-    dot_frame_binding = None
-if dot_frame_binding is not None and dot_frame_binding.is_configured():
-    dot_frame_binding.install_if_configured()
